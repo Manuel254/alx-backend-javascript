9611676380cdcc73dbedfdd5ebe15079c3fb8ec2
Manuel254 <Immanuelkituku@gmail.com>
2023-06-30 09:05:34 +0000

    FIx for...of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let [key, value] of array.entries()) {
-    array[key] = appendString + value;
+  const newArray = [];
+
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
