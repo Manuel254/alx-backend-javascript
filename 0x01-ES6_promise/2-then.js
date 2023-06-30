851937793f57b5fc48c085dc12fe59f338d37e64
Manuel254 <Immanuelkituku@gmail.com>
2023-06-30 10:52:50 +0000

    Return an empty error object

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,5 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({ status: 200, body: 'success' }))
-    .catch(() => {
-      throw new Error('');
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
