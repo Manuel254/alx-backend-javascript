78ffda62b5fdbc833a4e774c7ea7ac9f04c030b4
Manuel254 <Immanuelkituku@gmail.com>
2023-06-30 08:01:22 +0000

    Silence eslint for unused variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
+
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = false;
-    task2 = true;
+    const task = false;
+    const task2 = true;
   }
 
   return [task, task2];
