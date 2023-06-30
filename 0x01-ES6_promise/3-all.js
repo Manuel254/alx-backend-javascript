e0aa3683cce707063c591cbd7c07bbcd441097d3
Manuel254 <Immanuelkituku@gmail.com>
2023-06-30 11:09:53 +0000

    Factor console log to use string literals

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((items) => {
-    console.log(items[0].body, items[1].firstName, items[1].lastName);
-  }).catch(() => { console.log('Signup system offline'); };
+    console.log(`${items[0].body} ${items[1].firstName} ${items[1].lastName}`);
+  }).catch(() => { console.log('Signup system offline'); });
 }
