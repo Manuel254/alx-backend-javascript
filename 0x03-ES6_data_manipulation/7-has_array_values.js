a8f99b4afb602b62771d5960768fb3078c4cd531
Manuel254 <Immanuelkituku@gmail.com>
2023-07-04 12:34:17 +0000

    Fix eslint error

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  return array.map(num => set.has(num)).every(bool => bool === true);
+  return array.map((num) => set.has(num)).every((bool) => bool === true);
 }
