54c4db207b821f6f6773b97bbb051b57bec997bf
Manuel254 <immanuelkituku@gmail.com>
2023-08-22 12:51:36 +0000

    Read file asynchronously

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,7 +5,7 @@ function countStudents(path) {
     readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(Error('Cannot load the database'));
-      } else {   
+      } else {
         const numStudents = data.split('\n').length - 2;
         const lines = data.split('\n');
 
@@ -17,7 +17,7 @@ function countStudents(path) {
         }).map((str) => {
           const names = str.split(',');
           return names[0];
-        }); 
+        });
 
         const swe = lines.filter((str) => {
           const fields = str.split(',');
@@ -25,12 +25,12 @@ function countStudents(path) {
         }).map((str) => {
           const names = str.split(',');
           return names[0];
-        }); 
+        });
 
         console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
         console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
       }
-    }); 
+    });
   });
 }
 
