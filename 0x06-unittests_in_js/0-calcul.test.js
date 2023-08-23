05e85266240fc4d19e9ba1efd591cae1b88402e6
Manuel254 <immanuelkituku@gmail.com>
2023-08-23 16:47:26 +0000

    Add tests for calculation

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,44 +2,16 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function() {
-  it('should return 4', function() {
-    const result = calculateNumber(1, 3);
-    assert.equal(result, 4);
+  it('test round of a', function() {
+    assert.equal(calculateNumber(1.2, 4), 5);
+    assert.equal(calculateNumber(-1.5, 5), 4);
   });
-  it('should return 5', function() {
-    const result = calculateNumber(1, 3.7);
-    assert.equal(result, 5);
+  it('test round of b', function() {
+    assert.equal(calculateNumber(1, 4.5), 6);
+    assert.equal(calculateNumber(3, -5), -2);
   });
-  it('should return 5', function() {
-    const result = calculateNumber(1.2, 3.7);
-    assert.equal(result, 5);
-  });
-  it('should return 6', function() {
-    const result = calculateNumber(1.5, 3.7);
-    assert.equal(result, 6);
-  });
-  it('should return 3', function() {
-    const result = calculateNumber(-1, 4);
-    assert.equal(result, 3);
-  });
-  it('should return 0', function() {
-    const result = calculateNumber(-1, 0.5);
-    assert.equal(result, 0);
-  });
-  it('should return -4', function() {
-    const result = calculateNumber(-1, -3);
-    assert.equal(result, -4);
-  });
-  it('should return 3', function() {
-    const result = calculateNumber(-1, 3.7);
-    assert.equal(result, 3);
-  });
-  it('should return -4', function() {
-    const result = calculateNumber(-1, -3.7);
-    assert.equal(result, -4);
-  });
-  it('should return -5', function() {
-    const result = calculateNumber(-1.5, -3.7);
-    assert.equal(result, -5);
+  it('test round of both a and b', function() {
+    assert.equal(calculateNumber(1, 4), 5);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
